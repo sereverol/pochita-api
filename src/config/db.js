debd5865ffab0e9e1f93f3e57b372cb1e32d2f43
Silfrido Reverol <silfridoer@gmail.com>
2021-07-19 02:54:00 -0400

    db connection fields changed

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,21 @@
 module.exports = {
-  user: 'postgres',
-  host: 'localhost',
-  database: 'pochita',
-  password: 'asd123',
+  user: 'pkuklycazgcjah',
+  host: 'ec2-54-161-239-198.compute-1.amazonaws.com',
+  database: 'dbtda9o17arm7s',
+  password: 'fd38affc1b474af55c58c50e3c0e1dd3a63bb2b93f3eeffcd9082fcd085a03a5',
   port: 5432,
   max: 10,
   idleTimeoutMillis: 30000,
-  // ssl: { rejectUnauthorized: false },
+  ssl: { rejectUnauthorized: false },
 };
+
+// module.exports = {
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'pochita',
+//   password: 'asd123',
+//   port: 5432,
+//   max: 10,
+//   idleTimeoutMillis: 30000,
+//   ssl: { rejectUnauthorized: false },
+// };
